88fabf35d823f6bce06abebc56a0aa1d8b244957
Gema Parreño <gema.parreno.piqueras@gmail.com>
2022-09-14 14:35:51 +0200

    New script tokenizer2.py . testing

--- src/features/tokenizer.py
+++ src/features/tokenizer.py
@@ -14,8 +14,13 @@ pwd = pathlib.Path.cwd()
 
 from data import load_dataset
 
-## Instantiate this from data. Should go to main .
+
+#### TOKENIZER FOR EVERY LANGUAGE #####
+
+## Instantiate this from data. Should go to main . Load the dataset . the function
+# should be from local
 train_examples, val_examples = load_dataset(path) 
+
 
 def map_examples(train_examples):
     """
@@ -27,13 +32,14 @@ def map_examples(train_examples):
     train_lan = train_examples.map(lambda lan, en: lan)
     return train_lan, train_en
 
+bert_tokenizer_params=dict(lower_case=True)
 
 def vocab_generator():
     """
     generates a wordpiece vocabulary from a dataset
     see for more params generator
     """
-    bert_tokenizer_params=dict(lower_case=True)
+    
     reserved_tokens=["[PAD]", "[UNK]", "[START]", "[END]"]
 
     bert_vocab_args = dict(
@@ -50,17 +56,16 @@ def vocab_generator():
     
     train_lan, train_en = map_examples(train_examples)
     
-    lan_vocab = bert_vocab.bert_vocab_from_dataset(
-        train_lan.batch(1000).prefetch(2),
-        **bert_vocab_args
-    )
-#### Cell 57 
+
 
 def write_vocab_file(filepath, vocab):
   with open(filepath, 'w') as f:
     for token in vocab:
       print(token, file=f)
 
+vocabulary = vocab_generator()
+file_vocabulary_lan = write_vocab_file('vocabularylan.txt', **bert_tokenizer_params)
+file_vocabulary_en = write_vocab_file('vocabularyen.txt', **bert_tokenizer_params)
 
 ## Build the tokenizer
 
@@ -74,13 +79,49 @@ def tokenize(vocabularypath, bert_tokenizer_params):
     return language_tokenizer
 
 
+pt_tokenizer = text.BertTokenizer('vocabularylan.txt', **bert_tokenizer_params)
+en_tokenizer = text.BertTokenizer('vocabularyen.txt', **bert_tokenizer_params)
+
+### Instantiate the tokenizer 
+
+# Tokenize the examples -> (batch, word, word-piece)
+train_lan, train_en = map_examples(train_examples=train_examples)
+token_batch = en_tokenizer.tokenize(train_en)
+# Merge the word and word-piece axes -> (batch, tokens)
+token_batch = token_batch.merge_dims(-2,-1)
+# Lookup each token id in the vocabulary.
+txt_tokens = tf.gather(en_vocab, token_batch)
+# Join with spaces.
+tf.strings.reduce_join(txt_tokens, separator=' ', axis=-1)
+words = en_tokenizer.detokenize(token_batch)
+tf.strings.reduce_join(words, separator=' ', axis=-1)
+
+
+#Save Model
+def save_model(tokenizers, model_name):
+    """Save tokenizer model"""
+    model_name = 'ted_hrlr_translate_pt_en_converter'
+    return tf.saved_model.save(tokenizers, model_name)
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------#
 ## Build Tokenizer
 
-def tokenizer(model_name):
+def tokenizer_model_load(model_name):
     """
     Process of breaking up a sequence.
     The beginning of sentences are typically marked
-    by tokend
+    by tokens
 
     Apparently pt example is done and other language might imply going through the 
     full tutorial https://www.tensorflow.org/text/guide/subwords_tokenizer
@@ -104,22 +145,50 @@ def tokenizer(model_name):
     return tokenizers
 
 
-
-
-
-
-
-
-
 ### Add param to process. Without limitng the size of sequences, the performance
 # will be affected. 
 MAX_TOKENS = 128
 
+def prepare_token_batches(lan, en):
+    """
+    Tokenize per batches
 
+    """
+    model = tokenizer_model_load('ted_hrlr_translate_pt_en_converter')
+    lan = model.lan.tokenize(lan)
+    lan = lan[:, :MAX_TOKENS]
+    lan = lan.to_tensor()
+
+    en = model.en.tokenize(en)
+    en = en[:, :(MAX_TOKENS+1)]
+    en_inputs = en[:, :-1].to_tensor()  # Drop the [END] tokens
+    en_labels = en[:, 1:].to_tensor()   # Drop the [START] tokens
+
+    return (lan, en_inputs), en_labels
+
+BUFFER_SIZE = 20000
+BATCH_SIZE = 64
+
+def make_batches(ds):
+  return (
+      ds
+      .shuffle(BUFFER_SIZE)
+      .batch(BATCH_SIZE)
+      .map(prepare_token_batches, tf.data.AUTOTUNE)
+      .prefetch(buffer_size=tf.data.AUTOTUNE))
+
+
+    # Create training and validation set batches.
+train_batches = make_batches(train_examples)
+val_batches = make_batches(val_examples)
+
+#------------------------------------------------------#
 
 def filter_max_tokens(lan, en):
     num_tokens = tf.maximum(tf.shape(lan)[1],tf.shape(en)[1])
     return num_tokens < MAX_TOKENS
+
+    
 
 
 def tokenize_pairs(lan, en, model_name):
